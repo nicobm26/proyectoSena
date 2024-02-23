e38a0d5549bf334bc0b9cb3683ab396b64758085
nicolasb <nicolasbotellomendoza@gmail.com>
2024-02-23 10:17:57 -0500

    agrego la vista de contacto

---
+++ views/paginas/contacto.php
@@ -0,0 +1,34 @@
+<section class="banner">
+        <div class="contenido-banner">
+            <h1>Contáctenos</h1>
+        </div>
+    </section>
+
+    <main class="contenedor-contactanos">
+        <form class="formulario-contacto">                
+            <div class="campo-contacto">
+                <label for="nombre">Nombre</label>
+                <input type="text" placeholder="Tu Nombre" id="nombre" required>
+            </div>
+            <div class="campo-contacto">                
+                <label for="email">E-mail</label>
+                <input type="email" placeholder="Tu Email" id="email" required>
+            </div>
+            <div class="campo-contacto">
+                <label for="telefono">Teléfono</label>
+                <input type="tel" placeholder="Tu Teléfono" id="telefono" required>
+            </div>
+            <div class="campo-contacto">
+                <label for="mensaje">Mensaje:</label>
+                <textarea id="mensaje" required></textarea>
+            </div>
+            <div class="boton-enviar">
+                <input type="submit" value="Enviar" >
+            </div>
+        </form>
+
+        <div class="mapa">
+            <h3>Nuestra Ubicación</h3>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1975.9711241383318!2d-72.50482804564666!3d7.901102693168814!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e66456e988e59dd%3A0xf49d13d48187aef2!2sAv.%205%2C%20Barrio%20Pescadero%2C%20C%C3%BAcuta%2C%20Norte%20de%20Santander!5e0!3m2!1ses!2sco!4v1708619551679!5m2!1ses!2sco" width="600" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>     
+        </div>
+    </main>
