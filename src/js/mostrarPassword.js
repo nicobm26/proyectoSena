d29abc38c4ad4ea1bf1f7e9433f227ef680c2a1a
nicolasb <nicolasbotellomendoza@gmail.com>
2024-02-23 08:52:32 -0500

    mostrar la clave en registro

--- src/js/mostrarPassword.js
+++ src/js/mostrarPassword.js
@@ -2,10 +2,13 @@
 const showHiddenPass = (loginPass, loginEye) =>{
     const input = document.getElementById(loginPass),
           iconEye = document.getElementById(loginEye)
+   
+   if(!input && !iconEye) return;
  
     iconEye.addEventListener('click', () =>{
        // Change password to text
        if(input.type === 'password'){
+         console.log("mostrar contraseña en texto");
           // Switch to text
           input.type = 'text'
  
@@ -13,6 +16,7 @@ const showHiddenPass = (loginPass, loginEye) =>{
           iconEye.classList.add('ri-eye-line')
           iconEye.classList.remove('ri-eye-off-line')
        } else{
+         console.log("mostrar contraseña en astericos");
           // Change to password
           input.type = 'password'
  
@@ -21,6 +25,8 @@ const showHiddenPass = (loginPass, loginEye) =>{
           iconEye.classList.add('ri-eye-off-line')
        }
     })
- }
- 
+}
+
+showHiddenPass('clave','login-eye1');
+showHiddenPass('clave-repetida','login-eye2');
 showHiddenPass('login-pass','login-eye');
