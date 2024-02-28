5a262a14279c11c7e34927c14b01bcfa6392c177
nicolasb <nicolasbotellomendoza@gmail.com>
2024-02-28 11:06:55 -0500

    creo al admincontroller

---
+++ controllers/AdminController.php
@@ -0,0 +1,14 @@
+<?php 
+namespace Controllers;
+
+use MVC\Router;
+
+class AdminController{
+
+    public static function index( Router $router ){
+
+        $router->mostrarVista("admin/index",[
+        ]);
+    }
+
+}
