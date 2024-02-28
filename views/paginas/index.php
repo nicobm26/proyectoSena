4b5c37180615520806f4ddd5c1b3228feae70679
nicolasb <nicolasbotellomendoza@gmail.com>
2024-02-28 10:29:26 -0500

    vista hecha por nick

--- views/paginas/index.php
+++ views/paginas/index.php
@@ -1,5 +1,81 @@
-<!-- <main class="contenedor seccion"> -->
-    <h1>Holaaa amigos</h1>
-    <h2>Mas Sobre Nosotros</h2>
-<!-- </main> -->
+<section id="container-slider">
+    <a href="javascript: funcionEjecutar('anterior');" class="arrowPrev"><i class="fas fa-chevron-circle-left"></i></a>
+    <a href="javascript: funcionEjecutar('siguiente');" class="arrowNext"><i class="fas fa-chevron-circle-right"></i></a>
+    <ul class="listslider">
+        <li><a itlist="itList_0" href="#" class="item-select-slid"></a></li>
+        <li><a itlist="itList_1" href="#"></a></li>
+        <li><a itlist="itList_2" href="#"></a></li>
+    </ul>
+    <ul id="slider">
+        <li class="foto1">
+            <div class="content_slider">
+                <div>
+                    <h2>ApicolasGenesis.com</h2>
+                    <p>"La miel de la felicidad, ahora a un click de distancia."</p>
+                    <a href="" target="_blank" class="btnSlider">Iniciar Sesión</a>
+                </div>
+            </div>
+        </li>
+        <li class="foto2">
+            <div class="content_slider">
+                <div>
+                    <h2>ApicolasGenesis.com</h2>
+                    <p>"Haz clic, endúlzate. La mejor miel, ahora virtualmente tuya".</p>
+                    <a href="" target="_blank" class="btnSlider">Iniciar Sesión</a>
+                </div>
+            </div>
+        </li>
+        <li class="foto3">
+            <div class="content_slider">
+                <div>
+                    <h2>ApicolasGenesis.com</h2>
+                    <p>"Tu destino dulce está a un clic de distancia en nuestra tienda de miel online."</p>
+                    <a href="" target="_blank" class="btnSlider">Iniciar Sesión</a>
+                </div>
+            </div>
+        </li>
+    </ul>
+</section>
+<div class="contenedor">
+    <div class="caja-negra">
+        <div class="descripcion">
+            <h2 class="titulo_apicolas">Apicolas Genesis</h2>
+            <p>Bienvenido a ApicolasGenesis.com, tu destino premium para productos de miel excepcionales. En nuestra tienda, no solo ofrecemos miel de la más alta calidad, sino que también nos esforzamos por proporcionar una experiencia única y satisfactoria para nuestros clientes.</p>
+            <p>Descubre la exquisitez de nuestros productos, desde la miel pura y cruda hasta los subproductos de la colmena, como la propóleo y la jalea real. Cada artículo en nuestra tienda ha sido seleccionado con cuidado para garantizar no solo el sabor excepcional, sino también los beneficios para la salud que la miel y sus derivados pueden aportar a tu vida.</p>
+            <a href="#">
+                <button class="boton">Ver más</button>
+            </a>
+        </div>
+        <img src="img/productos.JPG" alt="Imagen de la sección">
+    </div>
+</div>
 
+<div class="caja">
+    <h2>Nuestros Productos Estrella</h2>
+    <div class="elementos">
+        <div class="elemento">
+            <div class="producto">
+                <div class="icono">
+                    <img src="img/propoleo.jpg" alt="Propóleo">
+                </div>
+                <p class="subtitulo2">Propóleo</p>
+            </div>
+        </div>
+        <div class="elemento">
+            <div class="producto">
+                <div class="icono">
+                    <img src="img/litro de miel2.jpg" alt="Miel">
+                </div>
+                <p class="subtitulo2">Miel</p>
+            </div>
+        </div>
+        <div class="elemento">
+            <div class="producto">
+                <div class="icono">
+                    <img src="img/polen2.jpg" alt="Jalea">
+                </div>
+                <p class="subtitulo2">Polen</p>
+            </div>
+        </div>
+    </div>
+</div>
