9a72e15d8ce898310097f6195184da82a207a7b3
nicolasb <nicolasbotellomendoza@gmail.com>
2024-02-29 09:49:21 -0500

    2 metodos agregados

--- models/Administrador.php
+++ models/Administrador.php
@@ -2,7 +2,7 @@
 
 namespace Model;
 
-class Administrador{
+class Administrador extends ActiveRecord{
     protected static $tabla = 'administrador';
     protected static $columnasDB = ['cedula','nombres', 'apellidos', 'correo', 'clave'];
 
@@ -14,10 +14,36 @@ class Administrador{
 
     public function __construct($args=[])
     {
-        $this->cedula = $args['cedula'];
-        $this->nombres = $args['nombres'];
-        $this->apellidos = $args['apellidos'];
-        $this->correo = $args['correo'];
-        $this->clave = $args['clave'];
+        $this->cedula = $args['cedula'] ?? null;
+        $this->nombres = $args['nombres'] ?? '';
+        $this->apellidos = $args['apellidos'] ?? '';
+        $this->correo = $args['correo'] ?? '';
+        $this->clave = $args['clave'] ?? '';
+    }
+
+    public function comprobarPassword($passwordUser){
+        $resultado = password_verify($passwordUser, trim($this->clave));
+        // debuguear($this->clave);
+        if( !$resultado){
+            self::$alertas['error'][] = "Clave Incorrecta";
+        }else{
+            return true;
+        }
+    }
+    
+    public function validarLogin(){
+        if(empty($this->correo) ){
+            self::$alertas['error'][] = 'El correo es obligatorio';
+        }else if(!filter_var($this->correo, FILTER_VALIDATE_EMAIL)) {
+            self::$alertas['error'][] = 'El correo no tiene el formato correcto';
+        }
+
+        if(!$this->clave){
+            self::$alertas['error'][] = 'La contraseña es obligatoria';
+        }
+        // else if( !preg_match("/^(?=(?:.*[A-Z]){1})(?=(?:.*[a-z]){5,})(?=(?:.*[0-9]){1})/", $this->clave)){
+        //     self::$alertas['error'][] = "La contraseña no es válida. Debe contener al menos 5 letras minúsculas, un número y una letra mayúscula.";
+        // }
+        return self::$alertas;
     }
 }
