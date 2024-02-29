4d0a72310a05cb59c621b690fa4a85622d5bec40
nicolasb <nicolasbotellomendoza@gmail.com>
2024-02-29 09:48:43 -0500

    creo el admincontroller

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -1,14 +1,35 @@
 <?php 
 namespace Controllers;
 
+use Model\Administrador;
 use MVC\Router;
 
 class AdminController{
 
-    public static function index( Router $router ){
-
+    public static function index(Router $router){
+        
         $router->mostrarVista("admin/index",[
+
         ]);
     }
+
+
+    // public static function crearAdmin(Router $router){
+    //     $cedula = 27840650;
+    //     $nombres = "maria";
+    //     $apellidos = "mendoza";
+    //     $correo = "maria@gmail.com";
+    //     $clave = password_hash("Maria10", PASSWORD_BCRYPT);
+    //     // debuguear( $clave);
+    //     $persona=[
+    //         'cedula'=>$cedula, 
+    //         'nombres'=>$nombres, 
+    //         'apellidos'=>$apellidos, 
+    //         'correo'=>$correo,
+    //         'clave'=>$clave
+    //     ];
+    //     $admin = new Administrador($persona);
+    //     $admin->guardarLLaveDefinida('cedula');   
+    // }
 
 }
