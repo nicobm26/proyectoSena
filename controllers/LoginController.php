aa69b7a1721250dbcac2b57c45a950dc81b741a9
nicolasb <nicolasbotellomendoza@gmail.com>
2024-02-29 18:01:29 -0500

    modifico algunas cosas

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -5,11 +5,12 @@ namespace Controllers;
 use Model\Usuario;
 use MVC\Router;
 use Classes\Email;
+use Model\Administrador;
 
 class LoginController{
 
-    public static function login(Router $router){
-        $errores = [];
+    public static function login(Router $router){        
+        $alertas = [];
         $auth = new Usuario($_POST);
 
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
@@ -29,20 +30,15 @@ class LoginController{
                         $_SESSION['cedula'] = $usuario->cedula;
                         $_SESSION['fullname'] = $usuario->nombres . " " . $usuario->apellidos;
                         $_SESSION['correo'] = $usuario->correo;
-                        $_SESSION['login'] = true;
+                        $_SESSION['login'] = true;                        
 
-                        //Redireccionamiento
-                        if($usuario->admin === "1"){
-                            $_SESSION['admin'] = $usuario->admin ?? null;
-                            header("Location: /admin");
-                        }else{
-                            header("Location: /cita");
-                        }            
+                        //Redireccionamiento   ///Re pensar, ya que nostros tenems            
+                        header("Location: /");      
                     }else{
                         Usuario::setAlerta('error', 'Clave incorrecta');
                     }
                 }else{
-                    Usuario::setAlerta('error', 'Usuario NO encontrado');
+                    Usuario::setAlerta('error', 'Correo no registrado');
                 }
             }
         }
@@ -50,7 +46,46 @@ class LoginController{
         $alertas = Usuario::getAlertas();
 
         $router->mostrarVista("/auth/login",[
-            "errores" => $errores,
+            "alertas" => $alertas,
+            "usuario" => $auth
+        ]);
+    }
+
+    public static function loginAdministrador(Router $router){
+        $alertas = [];
+        $auth = new Administrador();
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $auth = new Administrador($_POST);
+            $alertas = $auth->validarLogin();
+            if(empty($alertas)){    
+                //1.Comprobar que exista email
+                $admin = Administrador::where('correo', $auth->correo);
+                if($admin){
+                     //Existe Usuario, Proseguir a verificar password   
+                     if(  $admin->comprobarPassword($auth->clave)  ){
+                        //Autenticar el usuario
+                        if(!isset($_SESSION)) {  
+                            session_start();
+                        }
+                        $_SESSION['cedula'] = $admin->cedula;
+                        $_SESSION['fullname'] = $admin->nombres . " " . $admin->apellidos;
+                        $_SESSION['correo'] = $admin->correo;
+                        $_SESSION['login'] = true;
+                        $_SESSION['rol'] = 'admin';
+
+                        //Redireccionamiento   ///Re pensar, ya que nostros tenems            
+                        header("Location: /admin");      
+                    }else{
+                        Administrador::setAlerta('error', 'Clave incorrecta');
+                    }
+                }else{
+                    Administrador::setAlerta('error', 'Correo no registrado');
+                }
+            }
+        }
+        $alertas = Administrador::getAlertas();
+        $router->mostrarVista("/auth/login",[
+            "alertas" => $alertas,
             "usuario" => $auth
         ]);
     }
