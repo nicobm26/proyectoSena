e7ee79ad4476ae2eb5a088b2970584ecf1a661a2
nicolasb <nicolasbotellomendoza@gmail.com>
2024-02-29 17:57:08 -0500

    modifico modelo producto

--- models/Producto.php
+++ models/Producto.php
@@ -2,9 +2,9 @@
 
 namespace Model;
 
-class Producto{
+class Producto extends ActiveRecord{
     protected static $tabla = 'producto';
-    protected static $columnasBD = ['codigo','nombre','descripcion','precio','stock','imagen','codigoMedida'];
+    protected static $columnasDB = ['codigo','nombre','descripcion','precio','stock','imagen','codigoMedida','cedulaAdministrador'];
     
     public $codigo;
     public $nombre;
@@ -13,15 +13,43 @@ class Producto{
     public $stock;
     public $imagen;
     public $codigoMedida;
-
+    public $cedulaAdministrador;
+    
     public function __construct($args=[])
     {
-        $this->codigo = $args['codigo'];
-        $this->nombre = $args['nombre'];
-        $this->descripcion = $args['descripcion'];
-        $this->precio = $args['precio'];
-        $this->stock = $args['stock'];
-        $this->imagen = $args['imagen'];
-        $this->codigoMedida = $args['codigoMedida'];
+        $this->codigo = $args['codigo'] ?? null;
+        $this->nombre = $args['nombre'] ?? '';
+        $this->descripcion = $args['descripcion'] ?? '';
+        $this->precio = $args['precio'] ?? '';
+        $this->stock = $args['stock'] ?? '';
+        $this->imagen = $args['imagen'] ?? '';
+        $this->codigoMedida = $args['codigoMedida'] ?? '';
+        $this->cedulaAdministrador = $args['cedulaAdministrador'] ?? '';
+    }
+
+    public function setImagen($imagen){
+        if($imagen){
+            $this->imagen = $imagen;
+        }
+    }
+
+    public function validar(){
+        if(!$this->codigo)
+            self::$alertas['error'][] = 'Debe tener un codigo';
+
+        if(!$this->nombre)
+            self::$alertas['error'][] = 'El nombre es obligatorio';
+
+        if(!$this->descripcion)
+            self::$alertas['error'][] = 'Debes añadir una descripcion';           
+
+        if(!$this->precio)
+            self::$alertas['error'][] = 'El precio es obligatorio';
+
+        if(!$this->stock)
+            self::$alertas['error'][] = 'El stock es obligatorio';       
+
+        if(!$this->codigoMedida)
+            self::$alertas['error'][] = 'El codigo de medida es obligatorio';            
     }
 }
