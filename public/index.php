fd9cdce98594e6a52febddfb51f6916d1b737c97
nicolasb <nicolasbotellomendoza@gmail.com>
2024-02-29 18:00:51 -0500

    agrego nuevas urls

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\AdminController;
 use Controllers\LoginController;
 use MVC\Router;
 use Controllers\PaginasController;
@@ -24,8 +25,6 @@ $router->post("/contactanos", [PaginasController::class, 'contactanos']);
 // $router->post("/contactanos", [PaginasController::class, 'contactanos']);
 
 
-
-
 //Login y Autenticacion
 $router->get("/login", [LoginController::class, 'login']);
 $router->post("/login", [LoginController::class, 'login']);
@@ -36,6 +35,21 @@ $router->get("/logout", [LoginController::class, 'logout']);
 //Login admin
 $router->get("/loginAdmin", [LoginController::class, 'loginAdministrador']);
 $router->post("/loginAdmin", [LoginController::class, 'loginAdministrador']);
+
+//Panel de administracion
+$router->get("/admin", [AdminController::class, 'index']);
+$router->get("/producto/agregar", [AdminController::class, 'agregarProducto']);
+$router->post("/producto/agregar", [AdminController::class, 'agregarProducto']);
+$router->get("/producto/actualizar", [AdminController::class, 'actualizarProducto']);
+$router->post("/producto/actualizar", [AdminController::class, 'actualizarProducto']);
+$router->post("/producto/eliminar", [AdminController::class, 'eliminarProducto']);
+
+$router->get("/medida/agregar", [AdminController::class, 'agregarMedida']);
+$router->post("/medida/agregar", [AdminController::class, 'agregarMedida']);
+$router->get("/medida/actualizar", [AdminController::class, 'actualizarMedida']);
+$router->post("/medida/actualizar", [AdminController::class, 'actualizarMedida']);
+$router->post("/medida/eliminar", [AdminController::class, 'eliminarMedida']);
+// $router->get("/crearAdmin", [AdminController::class, 'crearAdmin']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
