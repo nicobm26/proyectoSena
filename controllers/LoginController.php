f9a4a1ce5375ae572c609bc8863627e0d636e7ab
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-01 16:31:12 -0500

    metodo logout

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -134,6 +134,13 @@ class LoginController{
         ]);
     }
 
+    public static function logout(){
+        session_start();
+        $_SESSION = [];
+        session_destroy();
+        header('Location: /');
+    }
+
 
 }
 ?>
