5b4ed39497bce4429e04123be1db47c6d2adb22e
Daniel Felipe <DanielFelipeRestrepoOrtega@gmail.com>
2024-03-04 15:51:49 -0500

    modifique vista de producto

--- views/paginas/producto.php
+++ views/paginas/producto.php
@@ -12,20 +12,25 @@
 
     <article class="CLick-segundo-article">
         <h2>Información</h2>
-        <p>Cantidad 
-            <select id="cantidad" name="cantidad">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="5">6</option>
-                <option value="5">7</option>
-                <option value="5">8</option>
-                <option value="5">9</option>
-                <option value="5">10+</option>
-            </select></p>
-                <button class="boton-Comprar">Comprar ahora</button><br>
+            <form action="/comprar" method="post">
+            <p>Cantidad 
+                <select id="cantidad" name="cantidad">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10+</option>
+                </select></p>
+                <input type="hidden" name="codigo" value="<?php echo $producto->codigo ?>">
+                <input type="submit" class="boton-Comprar" value="Comprar"> </input>
+            </form>
+            
+                <!-- <a href="/comprar?cantidad=">Comprar</a> -->
                 <button class="boton-Comprar">Agregar al carrito</button><br>
                 <a href="/productos"><button class="boton-Comprar">Volver</button></a>
     </article>
