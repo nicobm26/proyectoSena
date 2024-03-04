2ea5c1b3772d3a22ae74f0ee60f39fe457d5d2b8
Daniel Felipe <DanielFelipeRestrepoOrtega@gmail.com>
2024-03-04 17:44:17 -0500

    intentando hacer agregar a carrito

--- controllers/ComprarController.php
+++ controllers/ComprarController.php
@@ -10,19 +10,37 @@ class ComprarController{
     public static function comprar(Router $router){
         
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+            $codigo = $_POST['codigo'];
             $cantidad = $_POST['cantidad'];
-            $codigo =  $_POST['codigo'];
-            $producto = Producto::where('codigo', $codigo);
-            $total = $cantidad * $producto->precio;
-            // debuguear($producto);
+            
+            if (isset($_POST['accion'])) {
+                $accion = $_POST['accion'];
         
-    
+                if ($accion === 'Comprar') {
+                    // Se hizo clic en el botón "Comprar"
+                    $cantidad = $_POST['cantidad'];
+                    $codigo =  $_POST['codigo'];
+                    $producto = Producto::where('codigo', $codigo);
+                    $total = $cantidad * $producto->precio;
+
+                    $router->mostrarVista('paginas/comprar', [
+                        'cantidad' => $cantidad,
+                        'producto' => $producto,
+                        'total' => number_format($total,0,',','.')
+                    ]);
+                } elseif ($accion === 'AgregarCarrito') {
+                    // Se hizo clic en el botón "Agregar Carrito"
+                    //$carrito[]
+
+                }
+            }
+            
+            
+            // debuguear($producto);
         }
-        $router->mostrarVista('paginas/comprar', [
-            'cantidad' => $cantidad,
-            'producto' => $producto,
-            'total' => number_format($total,0,',','.')
-        ]);
+
+       
 
     }
 }
