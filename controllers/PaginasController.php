de614c195a04aad5e4e9fb943e4dece1e1d081e9
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-08 07:47:56 -0500

    modifico el metodo producto de PaginasController

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -35,10 +35,65 @@ class PaginasController{
     }
 
     public static function producto(Router $router){
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $codigo = $_POST['codigo'];
+            $cantidad = $_POST['cantidad'];
+            
+            if (isset($_POST['accion'])) {
+                $accion = $_POST['accion'];
+        
+                if ($accion === 'Comprar') {
+                    // Se hizo clic en el botón "Comprar"
+                
+                    $producto = Producto::where('codigo', $codigo);
+                    $total = $cantidad * $producto->precio;
+
+                    $router->mostrarVista('paginas/comprar', [
+                        'cantidad' => $cantidad,
+                        'producto' => $producto,
+                        'total' => number_format($total,0,',','.')
+                    ]);
+                } elseif ($accion === 'AgregarCarrito') {
+                    // Se hizo clic en el botón "Agregar Carrito"
+                    $producto = new Producto();
+                    $producto = Producto::where('codigo', $codigo);  
+                    if(! isset($_SESSION['login'])){
+                        $alertas['error'][] = 'Debes Iniciar Sesión';                    
+                    }else{                    
+                        if (!isset($_SESSION['carrito'])) {
+                            $_SESSION['carrito'] = array();
+                        }
+                        $encontrado = false;
+
+                        for ($i = 0; $i < count($_SESSION['carrito']); $i++) {
+                        $codigoActual = $_SESSION['carrito'][$i]['codigo'];
+                        
+                        if ($codigoActual === $codigo) {
+                            $_SESSION['carrito'][$i]['cantidad'] = $cantidad;
+                            $encontrado = true;
+                            break;
+                        }
+                        }
+
+                        if (!$encontrado) {
+                        array_push($_SESSION['carrito'], array("codigo" => $codigo, "cantidad" => $cantidad));
+                        }
+                        $alertas['exito'][] = 'Agregado al carrito';
+                    }
+                   
+                    $router->mostrarVista('paginas/producto', [                       
+                        'alertas' => $alertas,
+                        'producto' => $producto
+                    ]);
+                }
+            } 
+        }
+        $alertas=[];
         $codigo = $_GET['codigo'];
         $producto = Producto::where('codigo', $codigo);
         $router->mostrarVista("paginas/producto",[       
-            'producto'=> $producto
+            'producto'=> $producto,
+            'alertas'=> $alertas
         ]);
     }
 
