03ed857637334443f3a43a077f22b60b51bc5e23
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-08 09:45:45 -0500

    modifico el comprarController

--- controllers/ComprarController.php
+++ controllers/ComprarController.php
@@ -4,16 +4,45 @@ namespace Controllers;
 
 use MVC\Router;
 use Model\Producto;
+use Model\UnidadesMedida;
 
 class ComprarController{
 
-    public static function comprar(Router $router){
-                 
+    public static function comprar(Router $router){       
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $codigo = $_POST['codigo'];
+            $cantidad = $_POST['cantidad'];
+            $producto = Producto::where('codigo', $codigo);
+            $total = $cantidad * $producto->precio;
+        }
+        $router->mostrarVista('paginas/comprar', [
+            'cantidad' => $cantidad,
+            'producto' => $producto,
+            'total' => number_format($total,0,',','.')
+        ]);    
     }
 
     public static function carrito(Router $router){
-        $router->mostrarVista('paginasUsuario/carrito', [                       
+        $alertas=[];
+        $productos=[];
+        $medida = new UnidadesMedida();
+        if( !isset($_SESSION['carrito']) ) {
+           $alertas['error'][] = 'No hay productos agregados al carrito';
+        }else{
+            // debuguear($_SESSION['carrito'] );
+            foreach ($_SESSION['carrito'] as $producto) {
+                // Accedemos a los datos del producto actual
+                $codigo = $producto['codigo'];
+                $cantidad = $producto['cantidad'];
+                $producto = Producto::where('codigo', $codigo);
+                $productos[] = $producto;
+            }
+            
+        }
         
+        $router->mostrarVista('paginasUsuario/carrito', [                       
+            'alertas'=> $alertas ,
+            'productos' => $productos
         ]);    
     }
 }
