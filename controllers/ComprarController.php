dcd78a972591e67746bde1c5a6f8895814fea9af
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-08 07:47:37 -0500

    modifico comprarController

--- controllers/ComprarController.php
+++ controllers/ComprarController.php
@@ -8,40 +8,13 @@ use Model\Producto;
 class ComprarController{
 
     public static function comprar(Router $router){
+                 
+    }
+
+    public static function carrito(Router $router){
+        $router->mostrarVista('paginasUsuario/carrito', [                       
         
-        if($_SERVER['REQUEST_METHOD'] === 'POST'){
-
-            $codigo = $_POST['codigo'];
-            $cantidad = $_POST['cantidad'];
-            
-            if (isset($_POST['accion'])) {
-                $accion = $_POST['accion'];
-        
-                if ($accion === 'Comprar') {
-                    // Se hizo clic en el botón "Comprar"
-                    $cantidad = $_POST['cantidad'];
-                    $codigo =  $_POST['codigo'];
-                    $producto = Producto::where('codigo', $codigo);
-                    $total = $cantidad * $producto->precio;
-
-                    $router->mostrarVista('paginas/comprar', [
-                        'cantidad' => $cantidad,
-                        'producto' => $producto,
-                        'total' => number_format($total,0,',','.')
-                    ]);
-                } elseif ($accion === 'AgregarCarrito') {
-                    // Se hizo clic en el botón "Agregar Carrito"
-                    //$carrito[]
-
-                }
-            }
-            
-            
-            // debuguear($producto);
-        }
-
-       
-
+        ]);    
     }
 }
 
