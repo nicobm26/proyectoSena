de05aac2303f1fb2274d3d8fc433f13c8bde30c1
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-08 09:46:19 -0500

    agrego la cantidad y unidad  de forma dinamica

--- views/paginas/productos.php
+++ views/paginas/productos.php
@@ -3,12 +3,15 @@
 
 <section class="Cont-Productos">
     <?php
+    use Model\UnidadesMedida;
+    $unidad = new UnidadesMedida();
     foreach($productos as $producto){
+        $unidad = UnidadesMedida::where('codigo', $producto->codigoMedida);
         ?>
         <article class="cont-miel">
         <h2 class="titulo-producto"><?php echo $producto->nombre?></h2>
         <a href="/producto?codigo=<?php echo $producto->codigo?>"><img class="imgProductos" src="imagenes/<?php echo $producto->imagen?>" alt="<?php echo $producto->nombre?>"></a>
-        <p>750ml</p>
+        <p><?php echo $producto->cantidad ?> <?php echo $unidad->abreviatura; ?></p>
         <p><strong>Precio:</strong> <?php echo number_format($producto->precio, 0, ',' , '.')  ?> COP</p>
         <a href="/producto?codigo=<?php echo $producto->codigo?>"><button class="button-Producto">Ver Más</button></a>        
     </article>
