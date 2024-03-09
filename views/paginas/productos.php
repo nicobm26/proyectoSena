a1cf4c3432c17c6869ae9b4bacba5f01fa88f527
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-08 22:12:20 -0500

    intento de poner responsive la imagen pero no se logro

--- views/paginas/productos.php
+++ views/paginas/productos.php
@@ -8,9 +8,15 @@
     foreach($productos as $producto){
         $unidad = UnidadesMedida::where('codigo', $producto->codigoMedida);
         ?>
-        <article class="cont-miel">
+    <article class="cont-miel">
         <h2 class="titulo-producto"><?php echo $producto->nombre?></h2>
-        <a href="/producto?codigo=<?php echo $producto->codigo?>"><img class="imgProductos" src="imagenes/<?php echo $producto->imagen?>" alt="<?php echo $producto->nombre?>"></a>
+
+        <div class="contenedor-imagen">
+            <a href="/producto?codigo=<?php echo $producto->codigo?>">
+                <img class="imgProductos" src="imagenes/<?php echo $producto->imagen?>" alt="<?php echo $producto->nombre?>">
+            </a>            
+        </div>       
+       
         <p><?php echo $producto->cantidad ?> <?php echo $unidad->abreviatura; ?></p>
         <p><strong>Precio:</strong> <?php echo number_format($producto->precio, 0, ',' , '.')  ?> COP</p>
         <a href="/producto?codigo=<?php echo $producto->codigo?>"><button class="button-Producto">Ver Más</button></a>        
