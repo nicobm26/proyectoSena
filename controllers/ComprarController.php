a8810c7618b53f98a5b44f4d85ab61ca0f0f9489
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-09 12:35:33 -0500

    modifico el metodo comprar de ComprarController

--- controllers/ComprarController.php
+++ controllers/ComprarController.php
@@ -8,18 +8,25 @@ use Model\UnidadesMedida;
 
 class ComprarController{
 
-    public static function comprar(Router $router){       
+    public static function comprar(Router $router){   
+        $cantidad=[];
+        $producto=[];
+        $total="";
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
             $codigo = $_POST['codigo'];
             $cantidad = $_POST['cantidad'];
             $producto = Producto::where('codigo', $codigo);
             $total = $cantidad * $producto->precio;
+            $total = number_format($total,0,',','.');
+            $router->mostrarVista('paginas/comprar', [
+                'cantidad' => $cantidad,
+                'producto' => $producto,
+                'total' => $total
+            ]);    
+        }if($_SERVER['REQUEST_METHOD'] === 'GET'){
+            header('Location: /productos');
         }
-        $router->mostrarVista('paginas/comprar', [
-            'cantidad' => $cantidad,
-            'producto' => $producto,
-            'total' => number_format($total,0,',','.')
-        ]);    
+        
     }
 
     public static function carrito(Router $router){
